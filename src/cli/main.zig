d7d2791b3b4e318d00bb8ecd7870986c39d53559
Jassiel Ovando <jassielovando@protonmail.com>
2026-05-15 18:58:07 -0400

    Fix description punctuation and update fangz subproject commit reference

--- src/cli/main.zig
+++ src/cli/main.zig
@@ -21,8 +21,6 @@ pub fn main(init: std.process.Init) !void {
         .author_name = "",
         .author_email = "",
         .tagline = "A Documentation Linter for Zig Projects",
-        // TODO: Description should be inferred from the manifest (build.zig.zon), this should moved to the manifest, and inferred from there.
-        .description = "Docent is a documentation linter for Zig projects.",
     });
 
     defer app.deinit();
