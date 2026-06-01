053e712467721e6f06b0b953fbdd4ab51e0c0ee1
Jassiel Ovando <jassielovando@protonmail.com>
2026-06-01 19:10:39 -0400

    check todo and toml

--- src/cli/commands/check.zig
+++ src/cli/commands/check.zig
@@ -1,1 +1,11 @@
-//! Check will run all lint rules. It won't print diagnostics, but rather a simple summary, categorized by check type and severity, along with total number of found issues. It'll return a non-zero exit code if any issues are found, and zero if no issues are found.
+//! TODO: Check will run all lint rules. It won't print diagnostics, but rather a simple summary, categorized by check type and severity, along with total number of found issues. It'll return a non-zero exit code if any issues are found, and zero if no issues are found.
+//! The format I was thinking it's mostly:
+//! ```
+//! Documentation comments:
+//! - n <severity>[<rule_id>]
+//! Style:
+//! - n <severity>[<rule_id>]
+//! Complexity:
+//! - n <severity>[<rule_id>]
+//! ```
+
