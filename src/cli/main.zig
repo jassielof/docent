e743712dabb478eec8e14f9e4afc87398edf8fc9
Jassiel Ovando <jassielovando@protonmail.com>
2026-06-28 18:48:54 -0400

    fmt

--- src/cli/main.zig
+++ src/cli/main.zig
@@ -3,17 +3,17 @@ const std = @import("std");
 const fangz = @import("fangz");
 
 const check_command = @import("commands/check.zig");
+const fmt = @import("commands/fmt.zig");
 const init_command = @import("commands/init.zig");
 const status_command = @import("commands/status.zig");
-const fmt = @import("commands/fmt.zig");
+const types = @import("types.zig");
+
 pub const rule_config = @import("rule_config.zig");
-
-pub const registerStatusSubcommand = status_command.register;
 pub const registerConfigPathFlag = @import("flags.zig").registerConfigPath;
-
-pub const OutputMode = @import("types.zig").OutputMode;
-pub const FailFast = @import("types.zig").FailFast;
-pub const default_fail_fast = @import("types.zig").default_fail_fast;
+pub const default_fail_fast = types.default_fail_fast;
+pub const FailFast = types.FailFast;
+pub const OutputMode = types.OutputMode;
+pub const registerStatusSubcommand = status_command.register;
 
 pub fn main(init: std.process.Init) !void {
     const gpa = init.gpa;
