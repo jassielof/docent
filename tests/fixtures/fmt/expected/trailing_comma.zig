0bd984c4a9c5fbadbbf15f50f672c0d5cf357f2b
Jassiel Ovando <jassielovando@protonmail.com>
2026-06-27 23:30:36 -0400

    fix expected fixture

--- tests/fixtures/fmt/expected/trailing_comma.zig
+++ tests/fixtures/fmt/expected/trailing_comma.zig
@@ -1,11 +1,97 @@
+fn foo(
+    a: u8,
+    b: u8,
+    c: u8,
+) void {
+    _ = a;
+    _ = b;
+    _ = c;
+}
+
+fn bar(
+    x: u8,
+    y: u8,
+) void {
+    _ = x;
+    _ = y;
+}
+
+fn baz(
+    one: u8,
+    two: u8,
+    three: u8,
+    four: u8,
+) void {
+    _ = one;
+    _ = two;
+    _ = three;
+    _ = four;
+}
+
+fn quux(
+    a: u8,
+    b: u8,
+    c: u8,
+) void {
+    _ = a;
+    _ = b;
+    _ = c;
+}
+
 fn example() void {
-    foo(a, b, c,);
-    bar(x, y);
-    baz(one, two, three, four,);
-    const s = .{ .a = 1, .b = 2, .c = 3, };
-    const arr = [_]u8{ 1, 2, 3, };
-    const nested = foo(bar(a, b, c,), d, e,);
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+    foo(
+        a,
+        b,
+        c,
+    );
+
+    bar("x", "y");
+
+    baz(
+        "one",
+        "two",
+        "three",
+        "four",
+    );
+
+    const s = .{
+        .a = 1,
+        .b = 2,
+        .c = 3,
+    };
+    _ = s;
+
+    const arr = [_]u8{
+        1,
+        2,
+        3,
+    };
+    _ = arr;
+
+    const nested = foo(
+        bar(
+            "x",
+            "y",
+            "z",
+        ),
+        "d",
+        "e",
+    );
+    _ = nested;
+
     const two_fields = .{ .x = 1, .y = 2 };
+    _ = two_fields;
+
     const msg = "hello, world, foo";
-    quux(a, b, c,);
+    _ = msg;
+
+    quux(
+        a,
+        b,
+        c,
+    );
 }
