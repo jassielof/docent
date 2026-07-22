c02f7ef84f913acf1d460ea70759504bbd75e3cd
Jassiel Ovando <jassielovando@protonmail.com>
2026-07-22 11:45:48 -0400

    fmt and todo

--- cmd/docent/main.zig
+++ cmd/docent/main.zig
@@ -1,19 +1,20 @@
 const std = @import("std");
 
 const docent = @import("docent");
-pub const rule_config = docent.rule_config;
-pub const default_fail_fast = docent.types.default_fail_fast;
-pub const FailFast = docent.types.FailFast;
-pub const OutputMode = docent.types.OutputMode;
-pub const registerConfigPathFlag = docent.flags.registerConfigPath;
 const fangz = @import("fangz");
 
 const check_command = @import("check.zig");
 const fmt = @import("fmt.zig");
 const init_command = @import("init.zig");
 const status_command = @import("status.zig");
-pub const registerStatusSubcommand = status_command.register;
 const typeset_command = @import("typeset.zig");
+
+pub const default_fail_fast = docent.types.default_fail_fast;
+pub const FailFast = docent.types.FailFast;
+pub const OutputMode = docent.types.OutputMode;
+pub const registerConfigPathFlag = docent.flags.registerConfigPath;
+pub const rule_config = docent.rule_config;
+pub const registerStatusSubcommand = status_command.register;
 
 pub fn main(init: std.process.Init) !void {
     const gpa = init.gpa;
